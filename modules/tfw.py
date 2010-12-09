f3c80c2ba74349c860f4e1a5287d526f4a5bc76e
mutantmonkey <mutantmonkey@gmail.com>
2010-12-08 22:00:54 -0500

    Added documentation and Celsius support to .tfw

--- modules/tfw.py
+++ modules/tfw.py
@@ -10,14 +10,21 @@ from urllib import quote as urlquote
 from urllib2 import urlopen, HTTPError
 import lxml.html
 
-def tfw(phenny, input):
+def tfw(phenny, input, celsius=False):
+	""".tfw <city/zip> - Show the fucking weather at the specified location."""
+
 	zipcode = input.group(2)
 	if not zipcode:
 		# default to Blacksburg, VA
 		zipcode = "24060"
 
+	if celsius:
+		celsius_param = "&CELSIUS=yes"
+	else:
+		celsius_param = ""
+
 	try:
-		req = urlopen("http://thefuckingweather.com/?zipcode=%s" % urlquote(zipcode))
+		req = urlopen("http://thefuckingweather.com/?zipcode=%s%s" % (urlquote(zipcode), celsius_param))
 	except HTTPError:
 		phenny.say("THE INTERNET IS FUCKING BROKEN. Please try again later.")
 		return
@@ -50,6 +57,11 @@ def tfw(phenny, input):
 	response = "%s %s - %s - %s" % (temp, comment, remark, location)
 	phenny.say(response)
 tfw.rule = (['tfw'], r'(.*)')
+
+def tfwc(phenny, input):
+	""".tfwc <city/zip> - The fucking weather, in fucking celsius."""
+	return tfw(phenny, input, True)
+tfwc.rule = (['tfwc'], r'(.*)')
 
 if __name__ == '__main__':
 	print __doc__.strip()
