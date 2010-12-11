15ce88b20a8fadc6dc2bd795832705d11254c0ea
mutantmonkey <mutantmonkey@gmail.com>
2010-12-11 15:58:43 -0500

    Bot can now have too much food

--- modules/botsnack.py
+++ modules/botsnack.py
@@ -10,9 +10,16 @@ def botsnack(phenny, input):
 	messages = ["Om nom nom", "Delicious, thanks!"]
 	response = random.choice(messages)
 
-	phenny.say(response)
+	botsnack.snacks += 1
+
+	if botsnack.snacks % 7 == 0:
+		phenny.say("Too much food!")
+		phenny.do("explodes")
+	else:
+		phenny.say(response)
 botsnack.commands = ['botsnack']
 botsnack.priority = 'low'
+botsnack.snacks = 0
 
 if __name__ == '__main__':
 	print __doc__.strip()
