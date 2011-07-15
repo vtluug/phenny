d77fc7a8b0334e0e1fdcb410fabeb95d7a14b3ca
Casey Link <unnamedrambler@gmail.com>
2011-07-15 14:32:32 -0400

    New Module: nsfw
    Somethings just aren't safe work work.

---
+++ modules/nsfw.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+"""
+nsfw.py - some things just aren't safe for work, a phenny module
+author: Casey Link <unnamedrambler@gmail.com
+"""
+
+def nsfw(phenny, input):
+    link = input.group(2)
+    if not link:
+        phenny.say(".nsfw <link> - for when a link isn't safe for work")
+        return
+    phenny.say("!!NSFW!! -> %s <- !!NSFW!!" % (link))
+
+nsfw.rule = (['nsfw'], r'(.*)')
+
+if __name__ == '__main__':
+	print __doc__.strip()
+
