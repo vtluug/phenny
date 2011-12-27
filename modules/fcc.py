2db37270a74caff12e941b0b8220df461abf2370
mutantmonkey <mutantmonkey@mutantmonkey.in>
2011-12-26 22:16:16 -0500

    cleanup in fcc module

--- modules/fcc.py
+++ modules/fcc.py
@@ -9,26 +9,29 @@ import web
 import json
 
 def fcc(phenny, input):
-	""".fcc <callsign> - Look up a callsign issued by the FCC."""
+    """.fcc <callsign> - Look up a callsign issued by the FCC."""
 
-	callsign = input.group(2)
+    callsign = input.group(2)
+    if not callsign:
+        phenny.say(".fcc <callsign> - Look up a callsign issued by the FCC.")
+        return
 
-	try:
-		req = web.get("http://callook.info/{0}/json".format(web.quote(callsign)))
-		data = json.loads(req)
-	except (HTTPError, IOError, ValueError):
-		phenny.say("THE INTERNET IS FUCKING BROKEN. Please try again later.")
-		return
+    try:
+        req = web.get("http://callook.info/{0}/json".format(web.quote(callsign)))
+        data = json.loads(req)
+    except (HTTPError, IOError, ValueError):
+        phenny.say("THE INTERNET IS FUCKING BROKEN. Please try again later.")
+        return
 
-	if len(data) <= 0 or data['status'] == 'INVALID':
-	    phenny.reply('No results found for {0}'.format(callsign))
-	    return
+    if len(data) <= 0 or data['status'] == 'INVALID':
+        phenny.reply('No results found for {0}'.format(callsign))
+        return
 
-	response = "{0} - {1} - {2}".format(data['current']['callsign'],
-	        data['name'], data['otherInfo']['ulsUrl'])
-	phenny.say(response)
+    response = "{0} - {1} - {2}".format(data['current']['callsign'],
+            data['name'], data['otherInfo']['ulsUrl'])
+    phenny.say(response)
 fcc.rule = (['fcc'], r'(.*)')
 
 if __name__ == '__main__':
-	print(__doc__.strip())
+    print(__doc__.strip())
 
