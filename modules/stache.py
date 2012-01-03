e21b2f6ec76277ee141a42207b4e5d1384fbd270
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-01-03 15:02:57 -0500

    stache: fix case when no image is provided

--- modules/stache.py
+++ modules/stache.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
 stache.py - mustachify.me module
 author: mutantmonkey <mutantmonkey@mutantmonkey.in>
@@ -9,8 +10,13 @@ import web
 def stache(phenny, input):
     """.stache <url> - Mustachify an image."""
     url = input.group(2)
+    if not url:
+        phenny.reply("Please provide an image to Mustachify™.")
+        return
+
     phenny.reply('http://mustachify.me/?src=' + web.quote(url))
-stache.rule = (['stache'], r'(.*)')
+stache.rule = (['stache'],
+        '(https?:\/\/[^ #]+\.(?:png|jpg|jpeg))(?:[#]\.png)?')
 
 if __name__ == '__main__':
     print(__doc__.strip())
