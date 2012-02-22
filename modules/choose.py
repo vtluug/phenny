9ffdc281023eadff02b0ccffc88bf81460edc36e
Rebecca Stewart <res4v@virginia.edu>
2012-02-22 15:46:45 -0500

    fix .choose bugs

--- modules/choose.py
+++ modules/choose.py
@@ -11,12 +11,11 @@ def choose(phenny, input):
     origterm = input.groups()[1]
     if not origterm:
         return phenny.say(".choose <red> <blue> - for when you just can't decide")
-    origterm = origterm
-    c = re.findall(r'([^,]+)', input)
+    c = re.findall(r'([^,]+)', origterm)
     if len(c) == 1:
-        c = re.findall(r'(\S+)', input)
+        c = re.findall(r'(\S+)', origterm)
         if len(c) == 1:
-            return phenny.reply("%s" % (c))
+            return phenny.reply("%s" % (c.strip()))
     fate = random.choice(c).strip()
     return phenny.reply("%s" % (fate))
 choose.rule = (['choose'], r'(.*)')
