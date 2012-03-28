a951f0d3a70419337633d17bc3a4eef6b35604d0
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-03-27 23:20:18 -0400

    update linx module from andreim

--- modules/linx.py
+++ modules/linx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-linx.py - linx.li uploader
-author: mutantmonkey <mutantmonkey@mutantmonkey.in>
+linx.py - linx.li tools
+author: mutantmonkey <mutantmonkey@mutantmonkey.in>, andreim <andreim@andreim.net>
 """
 
 from urllib.error import HTTPError
@@ -29,6 +29,28 @@ def linx(phenny, input):
 
     phenny.reply(data['url'])
 linx.rule = (['linx'], r'(.*)')
+
+if __name__ == '__main__':
+    print(__doc__.strip())
+
+def lines(phenny, input):
+    """.lines <nickname> <date (YYYYMMDD)> - Returns the number of lines a user posted on a specific date."""
+
+    nickname = input.group(2)
+    date = input.group(3)
+    if not nickname or not date:
+        phenny.reply(".lines <nickname> <date (YYYYMMDD)> - Returns the number of lines a user posted on a specific date.")
+        return
+
+    try:
+        req = web.post("http://linx.li/vtluuglines", {'nickname': nickname, 'date': date})
+    except (HTTPError, IOError):
+        phenny.reply("THE INTERNET IS FUCKING BROKEN. Please try again later.")
+        return
+
+    phenny.reply(req)
+
+lines.rule = (['lines'], r'([a-z0-9\-_\\]+) (.*)')
 
 if __name__ == '__main__':
     print(__doc__.strip())
