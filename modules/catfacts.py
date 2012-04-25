037041cae40c77148c1f51d02e6c5e96386d46b2
Michael Barnes <code.mr.black@gmail.com>
2012-04-25 19:47:40 -0400

    added the cat facts command

---
+++ modules/catfacts.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import web
+
+def catfacts_ajax():
+    uri = 'http://facts.cat/getfact'
+    bytes = web.get(uri)
+    return web.json(bytes)
+
+def catfacts_get():
+    fact = catfacts_ajax()
+    try:
+        return fact['factoid']
+    except IndexError:
+        return None
+    except TypeError:
+        print(fact)
+        return False
+
+def catfacts(phenny, input):
+    fact = catfacts_get()
+    if fact:
+        phenny.reply(fact)
+catfacts.commands = ['catfacts']
+catfacts.priority = 'high'
