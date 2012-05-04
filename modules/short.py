2489b49639e49a43b9f2e103281b0d00f44dd3a3
Andrei M <aam@vt.edu>
2012-05-03 21:57:46 -0400

    short urls

---
+++ modules/short.py
@@ -0,0 +1,30 @@
+	#!/usr/bin/python3
+"""
+short.py - vtluug url shortner
+author: andreim <andreim@andreim.net>
+"""
+
+from urllib.error import HTTPError
+import web
+import json
+
+def short(phenny, input):
+    """.short <url> - Shorten a URL."""
+
+    url = input.group(2)
+    if not url:
+        phenny.reply("No URL provided. CAN I HAS?")
+        return
+
+    try:
+        req = web.post("http://vtlu.ug/vtluug", {'lurl': url})
+    except (HTTPError, IOError):
+        phenny.reply("THE INTERNET IS FUCKING BROKEN. Please try again later.")
+        return
+
+    phenny.reply(req)
+
+linx.rule = (['short'], r'(.*)')
+
+if __name__ == '__main__':
+    print(__doc__.strip())
