6aef5cab6f266bccb5d49c4d912ee374b23c3300
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-05-31 00:55:31 -0700

    add wuvt test

---
+++ modules/test/test_wuvt.py
@@ -0,0 +1,25 @@
+"""
+test_wuvt.py - tests for the wuvt module
+author: mutantmonkey <mutantmonkey@mutantmonkey.in>
+"""
+
+# add current working directory to path
+import sys
+sys.path.append('.')
+
+import re
+import unittest
+from mock import MagicMock, Mock
+from modules.wuvt import wuvt
+
+class TestWuvt(unittest.TestCase):
+    def setUp(self):
+        self.phenny = MagicMock()
+
+    def test_wuvt(self):
+        wuvt(self.phenny, None)
+
+        out = self.phenny.reply.call_args[0][0]
+        m = re.match('^DJ .* is currently playing: .* by .*$', out,
+                flags=re.UNICODE)
+        self.assertTrue(m)
