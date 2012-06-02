875d6c2b2a6f91531d5d080561647b2a23569041
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-06-01 22:33:13 -0700

    convert tools.py to 4 space indent

--- tools.py
+++ tools.py
@@ -13,19 +13,19 @@ class GrumbleError(Exception):
 
 
 def deprecated(old): 
-   def new(phenny, input, old=old): 
-      self = phenny
-      origin = type('Origin', (object,), {
-         'sender': input.sender, 
-         'nick': input.nick
-      })()
-      match = input.match
-      args = [input.bytes, input.sender, '@@']
+    def new(phenny, input, old=old): 
+        self = phenny
+        origin = type('Origin', (object,), {
+            'sender': input.sender, 
+            'nick': input.nick
+        })()
+        match = input.match
+        args = [input.bytes, input.sender, '@@']
 
-      old(self, origin, match, args)
-   new.__module__ = old.__module__
-   new.__name__ = old.__name__
-   return new
+        old(self, origin, match, args)
+    new.__module__ = old.__module__
+    new.__name__ = old.__name__
+    return new
 
 if __name__ == '__main__': 
-   print(__doc__.strip())
+    print(__doc__.strip())
