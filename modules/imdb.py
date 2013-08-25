cac9258c864f72acd978366899b5b63b882ff5e8
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-08-25 14:32:54 -0700

    imdb: switch to omdbapi.com domain

--- modules/imdb.py
+++ modules/imdb.py
@@ -15,18 +15,23 @@ def imdb_search(query):
    query = query.replace('!', '')
    query = query.encode('utf-8')
    query = web.quote(query)
-   uri = 'http://www.imdbapi.com/?i=&t=%s' % query
+   uri = 'http://www.omdbapi.com/?i=&t=%s' % query
    bytes = web.get(uri)
    m = json.loads(bytes)
    return m
 
 def imdb(phenny, input): 
    query = input.group(2)
-   if not query: return phenny.reply('.imdb what?')
+   if not query:
+       return phenny.say('.imdb what?')
 
    m = imdb_search(query)
    try:
-       phenny.reply('{0} ({1}): {2}  http://imdb.com/title/{3}'.format(m['Title'], m['Year'], m['Plot'], m['imdbID']))
+       phenny.say('{0} ({1}): {2}  http://imdb.com/title/{3}'.format(
+           m['Title'],
+           m['Year'],
+           m['Plot'],
+           m['imdbID']))
    except:
         phenny.reply("No results found for '%s'." % query)
 imdb.commands = ['imdb']
