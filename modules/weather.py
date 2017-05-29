9e8899bce100917bd5805968cc43a9cc418f391e
Paul Walko <paulsw.pw@gmail.com>
2017-05-29 17:28:14 -0400

    remove old code & use format

--- modules/weather.py
+++ modules/weather.py
@@ -17,14 +17,11 @@ r_from = re.compile(r'(?i)([+-]\d+):00 from')
 
 
 def location(q):
-    uri = 'https://nominatim.openstreetmap.org/search?%s={query}&format=json'
+    uri = 'https://nominatim.openstreetmap.org/search?{type}={query}&format=json'
     if q.isdigit():
-        uri = uri % 'postalcode'
+        uri = uri . format(type = 'postalcode', query = web.quote(q))
     else:
-        uri = uri % 'q'
-    uri = uri . format(query = web.quote(q))
-#    uri = 'https://nominatim.openstreetmap.org/search/?q={query}&format=json'.\
-#        format(query=web.quote(q))
+        uri = uri . format(type = 'q', query = web.quote(q))
     results = web.get(uri)
     data = json.loads(results)
 
